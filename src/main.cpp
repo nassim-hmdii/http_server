059933bda9ab433b4948c80efd9a7de205711e17
Nassim <nassimhamidi.unistudy@gmail.com>
2026-09-24 19:33:27 +0800

    Create and bind socket using resolved address info

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <cstring>
+#include <errno.h>
 
 int main(){
 // Fetching the addrress info thats gonna be used by the socket API 
-
     struct addrinfo hints{};   //call the stucture a zero it
     addrinfo * servaddrinfo;    // set the pointer to the result linked list
     // Setting the 3 members values 
@@ -19,15 +20,24 @@ int main(){
     std::cerr<< "getaddrinfo error " <<gai_strerror(result) << '\n' ;
     return(1);
     }
-    std::cout << "Address family: " << servaddrinfo->ai_family << '\n'; // if the function successful, print the address family of the first result in the linked list
+    // Creating the socket and the socket descriptor for futher operations done by the kernel
+    int sockfd;
+    int bindres;
 
+    sockfd = socket(servaddrinfo -> ai_family, servaddrinfo -> ai_socktype, servaddrinfo -> ai_protocol);
+    if(sockfd == -1){
+        std::cerr<< "Socket Error: "<< strerror(errno) << '\n';
+        return(1);
+    }
 
+    // Binding the socket to the port that was set using the socket descriptor
+    bindres = bind(sockfd, servaddrinfo -> ai_addr, servaddrinfo ->ai_addrlen);
+    if(bindres == -1){
+        std::cerr<< "Bind Error: "<< strerror(errno) << '\n';
+        return(1);
+    }
 
-
-
-
-
-
+    std::cout << "Socket created and binded successfully" << '\n';
     freeaddrinfo(servaddrinfo);
     return 0;
 }
